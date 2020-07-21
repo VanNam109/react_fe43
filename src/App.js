25d24732362fed0e88defde9a20ebcd65aeb6dab
VanNam109 <nguyennam109198@gmail.com>
2020-07-21 21:47:39 +0700

    buoi4-lifeupState

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/Header';
-import BaiTapLayout from './Components/BaiTapLayout/BaiTapLayout';
-import BaiTapLayout2 from './Components/BaiTapLayout2/BaiTapLayout2';
-import DataBinding from './DataBinding/DataBinding';
-import EventBinding from './EventBinding/EventBinding';
-import RenderWithState from './RenderWithState/RenderWithState';
-import BaiTapChonXe from './BaiTapChonXe/BaiTapChonXe';
-import RenderWithMap from './RenderWithMap/RenderWithMap';
-import DemoProps from './Props/DemoProps';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header";
+import BaiTapLayout from "./Components/BaiTapLayout/BaiTapLayout";
+import BaiTapLayout2 from "./Components/BaiTapLayout2/BaiTapLayout2";
+import DataBinding from "./DataBinding/DataBinding";
+import EventBinding from "./EventBinding/EventBinding";
+import RenderWithState from "./RenderWithState/RenderWithState";
+import BaiTapChonXe from "./BaiTapChonXe/BaiTapChonXe";
+import RenderWithMap from "./RenderWithMap/RenderWithMap";
+import DemoProps from "./Props/DemoProps";
+import ExampleCard from "./LiftingUpState/ExampleCard";
 
 function App() {
   return (
     <div className="App">
-        <BaiTapLayout />
-        {/* <BaiTapLayout2 /> */}
-        {/* <DataBinding />
+      {/* <BaiTapLayout /> */}
+      {/* <BaiTapLayout2 /> */}
+      {/* <DataBinding />
         <h1 className="header">hello fe43</h1> */}
-        {/* <EventBinding/> */}
-        {/* <RenderWithState/> */}
-        {/* <BaiTapChonXe/> */}
-        {/* <RenderWithMap/> */}
-        {/* <DemoProps/> */}
-        
+      {/* <EventBinding/> */}
+      {/* <RenderWithState/> */}
+      {/* <BaiTapChonXe/> */}
+      {/* <RenderWithMap/> */}
+      {/* <DemoProps/> */}
+      <ExampleCard />
     </div>
   );
 }
---
+++ src/LiftingUpState/ExampleCard.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+import data from "./data.json";
+import Product from "./Product";
+import ProductList from "./ProductList";
+import Card from "./Card";
+export default class ExampleCard extends Component {
+  state = {
+    proList: data,
+    proSelected: null,
+    cardList: [],
+  };
+
+  handleDelete = (card) => {
+    /**
+     * 1/tìm vị trí
+     * 2/cắt ra khỏi mảng
+     * 3/setState
+     */
+    let cardListUpdate = [...this.state.cardList];
+    let index = cardListUpdate.findIndex((item) => item.maSP === card.maSP);
+    if (index >= 0) {
+      cardListUpdate.splice(index, 1);
+      this.setState({
+        cardList: cardListUpdate,
+      });
+    }
+  };
+  handleSelectPro = (pro) => {
+    this.setState({
+      proSelected: pro,
+    });
+  };
+  handleBuy = (card) => {
+    //tạo cardList mới
+    // this.state.cardList.push(card);
+    let cardListUpdate = [...this.state.cardList];
+
+    //tìm vị trí phần tử trong mảng
+    let index = cardListUpdate.findIndex((item) => item.maSP === card.maSP);
+
+    if (index >= 0) {
+      //tìm thấy
+      cardListUpdate[index].soLuong += 1;
+    } else {
+      //Không tìm thấy
+      card.soLuong = 1;
+      cardListUpdate = [...cardListUpdate, card];
+    }
+    //set state
+    this.setState({
+      cardList: cardListUpdate,
+    });
+  };
+  renderProList = () => {
+    return this.state.proList.map((item, index) => {
+      return (
+        <div className="col-sm-4" key={index}>
+          <Product handleSelectPro={this.handleSelectPro} item={item} />
+        </div>
+      );
+    });
+  };
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <Card
+            handleDelete={this.handleDelete}
+            cardList={this.state.cardList}
+          />
+          <ProductList
+            handleSelectPro={this.handleSelectPro}
+            proList={this.state.proList}
+            handleBuy={this.handleBuy}
+          />
+
+          {this.state.proSelected ? (
+            <div className="row">
+              <div className="col-sm-5">
+                <img
+                  className="img-fluid"
+                  src={this.state.proSelected.hinhAnh}
+                />
+              </div>
+              <div className="col-sm-7">
+                <h3>Thông số kỹ thuật</h3>
+                <table className="table">
+                  <tbody>
+                    <tr>
+                      <td>Màn hình</td>
+                      <td>{this.state.proSelected.manHinh}</td>
+                    </tr>
+                    <tr>
+                      <td>Hệ điều hành</td>
+                      <td>{this.state.proSelected.heDieuHanh}</td>
+                    </tr>
+                    <tr>
+                      <td>Camera trước</td>
+                      <td>{this.state.proSelected.cameraTruoc}</td>
+                    </tr>
+                    <tr>
+                      <td>Camera sau</td>
+                      <td>{this.state.proSelected.cameraSau}</td>
+                    </tr>
+                    <tr>
+                      <td>RAM</td>
+                      <td>{this.state.proSelected.ram}</td>
+                    </tr>
+                    <tr>
+                      <td>ROM</td>
+                      <td>{this.state.proSelected.rom}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    );
+  }
+}
